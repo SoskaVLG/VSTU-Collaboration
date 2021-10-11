dcb863bb81d6efdb7bbb944b744a676983fecb65
Krykov Alexandr <57997351+SoskaVLG@users.noreply.github.com>
2021-10-11 18:26:32 +0300

    Update Task_1.cpp

--- Сашко/Laba (OA)/Task_1.cpp
+++ Сашко/Laba (OA)/Task_1.cpp
@@ -3,25 +3,25 @@
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "rus"); 
-    bool L, A, B, C;
+    setlocale(LC_ALL, "rus");
+    float L, A, B, C;
     float x, y;
-    int k; 
+    int k;
     float pi = 3.14;
 
     cout << "Введите x \n> ";
     cin >> x;
-    cout << "Введите y \n> "; 
-    cin >> y; 
+    cout << "Введите y \n> ";
+    cin >> y;
     cout << "Введите k \n> ";
     cin >> k;
 
-    A = (x+2*y<=2);
-    B = (5%k==0);
+    A = (x + 2 * y <= 2);
+    B = (5 % k == 0);
     C = pow(2.71, 2 * pi * k);
     L = (!(A == (!B)) && C) || (B && B);
 
     if (L == 0)cout << "FALSE (0)";
-    else cout << "TRUE (1)"; 
+    else cout << "TRUE (1)";
     return 0;
 }
