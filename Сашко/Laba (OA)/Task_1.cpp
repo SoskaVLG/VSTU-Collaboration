794ed2dcbb6fc4cb46c523fa9459a88374cf3ba0
SoskaVLG <krykov976@gmail.com>
2021-10-12 11:25:25 +0300

    Update Task_1.cpp

--- Сашко/Laba (OA)/Task_1.cpp
+++ Сашко/Laba (OA)/Task_1.cpp
@@ -1,27 +1,32 @@
-#include <iostream>
+﻿#include <iostream>
 #include <math.h>
 using namespace std;
 
 int main() {
-    setlocale(LC_ALL, "rus"); 
-    bool L, A, B, C;
+    setlocale(LC_ALL, "rus");
+    float L, A, B, C;
     float x, y;
-    int k; 
-    float pi = 3.14;
+    int k;
 
     cout << "Введите x \n> ";
     cin >> x;
-    cout << "Введите y \n> "; 
-    cin >> y; 
-    cout << "Введите k \n> ";
+    cout << "\nВведите y \n> ";
+    cin >> y;
+    cout << "\nВведите k \n> ";
     cin >> k;
+    if (k == 0) 
+    {
+        cout << "\nk = 0 , на ноль делить нельзя\n";
+        system("pause"); 
+        return 0; 
+    }
 
-    A = (x+2*y<=2);
-    B = (5%k==0);
-    C = pow(2.71, 2 * pi * k);
+    A = (x + 2 * y <= 2);
+    B = (5 % k == 0);
+    C = pow(2.71, 2 * 3.14 * k);
     L = (!(A == (!B)) && C) || (B && B);
 
-    if (L == 0)cout << "FALSE (0)";
-    else cout << "TRUE (1)"; 
+    if (L==0)cout << "FALSE (0)";
+    else cout << "TRUE (1)";
     return 0;
 }
