9625db14077c35ddcba2aa262b055222c4a89d99
SoskaVLG <krykov976@gmail.com>
2021-10-21 21:36:30 +0300

    Update Task_1.cpp

--- Сашко/Laba (inform)/№4/Task_1.cpp
+++ Сашко/Laba (inform)/№4/Task_1.cpp
@@ -1,41 +1,46 @@
 #include <iostream>
-using namespace std; 
+using namespace std;
 
 int main()
 {
-	setlocale(LC_ALL, "rus"); 
+	setlocale(LC_ALL, "rus");
 
-	float B[100] ; 
-	int n , i , index_min;
+	float B[100];
+	int n, i;
 	float min;
-	
+
 	cout << "\n Введите количество элементов массива\n> ";
-	cin >> n ;
+	cin >> n;
 
-	if (n < 5) 
+	if (n < 5)
 	{
-	cout << "Данная длина массива не подходит\n";
-	system("pause"); 
-	return 0;
+		cout << "Количество элементов меньше пяти\n";
+		system("pause"); 
+		return 0;
 	}
+
+	else 
+	{
 	
-	for (i = 0; i < n; i++)
-	{
-		cout << "\n B [" << i << "] = ";
-		cin >> B[i]; 
-	}
+		for (i = 0; i < n; i++)
+		{
+			cout << "\n B [" << i << "] = ";
+			cin >> B[i];
+		}
 
-	min = B[0]; 
-	for (i = 1; i < n; i++)
-	{
-		if (B[i] < min && B[i] >= -5 && B[i] <= 5) min = B[i];
-	}
+		min = B[0];
+		for (i = 1; i < n; i++)
+		{
+			if (B[i] < min && B[i] >= -5 && B[i] <= 5) min = B[i];
+		}
 
-	if (min > 0) for (i = 0; i < 5; i++) B [i] = min;
+		if (min > 0) for (i = 0; i < 5; i++) B[i] = min;
+
+	}
 	
 	system("cls");
 
-	for (i = 0; i < n; i++) cout << "\nB [" << i << "] = " << B[i]; 
+	for (i = 0; i < n; i++) cout << "\nB [" << i << "] = " << B[i];
 
-	return 0; 
+	return 0;
 }
