c2c3c4c00a7db0719f0ebd70730b48b0c336baef
SoskaVLG <krykov976@gmail.com>
2021-10-26 11:08:49 +0300

    Update Task_1 v2.cpp

--- Сашко/Laba (OA)/№2/Task_1 v2.cpp
+++ Сашко/Laba (OA)/№2/Task_1 v2.cpp
@@ -6,25 +6,26 @@ int main()
 {
 	setlocale(LC_ALL, "rus");
 	double elements[100];		// массив для элементов последовательности 
-	int n, i;					// n - кол-во элементов последовательности, i - переменная цикла
-	double ai = 0;				// элемент , вычисляющийся на каждой итерации цикла
-	double sum = 0;				// сумма элеметов 
 	elements[0] = 0;
-	elements[1] = 0; 
+	elements[1] = 0;
 	elements[2] = 1;
 	elements[3] = 2;
+	int n, i;				// n - кол-во элементов последовательности, i - переменная цикла
+	double ai = 0;				// элемент , вычисляющийся на каждой итерации цикла
+	double sum = 0;				// сумма элеметов 
 
 	cout << "Введите n \n> ";
-	cin >> n; 
+	cin >> n;
 
-	for (i = 4; i <=n; i++)
+	for (i = 4; i <= n; i++)
 	{
-		ai = elements[i - 2] + i * sqrt(elements[i - 1]) + 2 * elements[i - 3]; 
-		elements[i] = ai; 
+		ai = elements[i - 2] + i * sqrt(elements[i - 1]) + 2 * elements[i - 3];
+		elements[i] = ai;
 	}
-	for (i = 1; i <=n; i++)
+	for (i = 1; i <= n; i++)
 	{
-		sum += elements[i]; 
+		sum += elements[i];
 	}
-	cout << "\nСумма равна : " << sum; 
+	cout << "\nСумма равна : " << sum;
+	return 0; 
 }
