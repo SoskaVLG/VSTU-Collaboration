b41e08785dd32129e7b761d0f5f67eff99de8b5c
QuitePixel <silentpxl@gmail.com>
2021-11-14 19:04:37 +0300

    Create Semestr№2.cpp

---
+++ Микитко/Semestr work/Semestr№2.cpp
@@ -0,0 +1,13 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+void main()
+{
+	float F, x;
+	for (float x = 0.1; x < 8.1; x += 0.5)
+	{
+		F = 7.14 * pow(tan(2 * x), 2);
+		cout << "\nF=" << F << "\tx=" << x << endl;
+	
+	}
+}
