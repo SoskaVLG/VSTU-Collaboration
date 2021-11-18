cab84a646d3c30deacc7997c3c7611f857278973
SoskaVLG <krykov976@gmail.com>
2021-11-18 20:21:05 +0300

    Update Task#2.cpp

--- Сашко/Laba (inform)/№5/Task#2.cpp
+++ Сашко/Laba (inform)/№5/Task#2.cpp
@@ -1,18 +1,18 @@
 #include <iostream>
 #include <math.h>
 
-using namespace std; 
+using namespace std;
 
-int main () 
+int main()
 {
 	setlocale(LC_ALL, "rus");
-	int N, M, i, j, negative_counter = 0 , negative_i = 0, negative_j = 0, imin = 0, jmin = 0;
-	float A[50][50], min , second_negative_number;
+	int N, M, i, j, negative_counter = 0, negative_i = 0, negative_j = 0, imin = 0, jmin = 0 , current_j;
+	float A[50][50], min, second_negative_number;
 	cout << "\n Введите количество строк в массиве\n> ";
 	cin >> N;
 	cout << "\n Введите количество столбцов в массиве\n> ";
 	cin >> M;
-	
+
 	// ввод матрицы
 	for (j = 0; j < M; j++)
 	{
@@ -22,9 +22,9 @@ int main ()
 			cin >> A[i][j];
 		}
 	}
-	system("cls"); 
+	system("cls");
 	// вывод исходной матрицы
-	cout << "\n\nИсходная матрица : \n"; 
+	cout << "\n\nИсходная матрица : \n";
 	for (j = 0; j < M; j++)
 	{
 		for (i = 0; i < N; i++)
@@ -34,10 +34,10 @@ int main ()
 	}
 	// общий внешний цикл по столбцам
 	for (j = 0; j < M; j++)
-	{	
+	{ 
 		min = A[0][j];
 		negative_counter = 0;
-		
+
 		// внутренний цикл по сторокам для поиска минимума в стобце
 		for (i = 0; i < N; i++)
 		{
@@ -46,10 +46,10 @@ int main ()
 			{
 				min = A[i][j];
 				imin = i;
-				jmin = j; 
 			}
 		}
-		min = A[imin][jmin];
+		min = A[imin][j];
+		jmin = j;
 		// внутренний цикл для поиска второго отрицательного числа в столбце
 		for (i = 0; i < N; i++)
 		{
@@ -60,24 +60,24 @@ int main ()
 				{
 					negative_i = i;
 					negative_j = j;
-					
+
 					second_negative_number = A[negative_i][negative_j];
 					A[imin][jmin] = second_negative_number;
-					A[negative_i][negative_j] = min;	
+					A[negative_i][negative_j] = min;
 				}
 			}
 		}
 	}
 
 	// вывод конечной матрицы
-	cout << "\n\nКонечная матрица : \n"; 
+	cout << "\n\nКонечная матрица : \n";
 	for (j = 0; j < M; j++)
 	{
-		for (i= 0; i < N; i++)
+		for (i = 0; i < N; i++)
 		{
-			cout << "\nA [ " << i << "," << j << "] = " << A [i][j];
+			cout << "\nA [ " << i << "," << j << "] = " << A[i][j];
 		}
 	}
 
-	return 0; 
+	return 0;
 }
