378b96edb2e19e810a7c5607b038c5ecdf15e6c8
QuitePixel <silentpxl@gmail.com>
2021-11-23 10:58:25 +0300

    Create Laba2.cpp

---
+++ Микитко/Fundamentals of algorithmization/Laba2.cpp
@@ -0,0 +1,47 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	int const size = 100;
+	int A[size];
+	int n, i, counter, k;
+	cout << "\nВведите размерность массива n" << endl;
+	cin >> n;
+	while (n > 100)
+	{
+		cout << "Размерность массива превышает 100\nВведите значение n" << endl;
+		cin >> n;
+	}
+	for (i = 0; i < n; i++)
+	{
+		cout << "A[" << i << "]=";
+		cin >> A[i];
+		cout << "\n";
+	}
+	cout << "Вывод исходного массива" << endl;
+	for (i = 0; i < n; i++)
+	{
+		cout << A[i] << "\t";
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (A[i] % 2 == 0)
+		{
+			k = A[i];
+			counter = i;
+			i = n;
+
+		}
+	}
+	for (i = n - counter; i < n; i++)
+	{
+		A[i] *= k;
+	}
+	cout << "\nВывод измененного массива\n";
+	for (i = 0; i < n; i++)
+	{
+		cout << A[i] << "\t";
+	}
+}
