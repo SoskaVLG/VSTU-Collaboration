743afaea43e64cbf9a2abfd2cedaa2bfa71a02bb
QuitePixel <silentpxl@gmail.com>
2021-11-23 10:36:04 +0300

    Create Alg.Laba1.cpp

---
+++ Микитко/Fundamentals of algorithmization/Alg.Laba1.cpp
@@ -0,0 +1,45 @@
+﻿#include <iostream>
+#include <math.h>
+using namespace std;
+void main()
+{
+	setlocale(LC_ALL, "ru");
+	int const size = 100;
+	int A[size];
+	int n, i;
+	cout << "\nВведите размерность массива n" << endl;
+	cin >> n;
+	while (n > 100)
+	{
+		cout << "Размерность массива превышает 100\nВведите значение n" << endl;
+		cin >> n;
+	}
+	for (i = 0; i < n; i++)
+	{
+		cout << "A[" << i << "]=";
+		cin >> A[i];
+		cout << "\n";
+	}
+	cout << "Вывод исходного массива" << endl;
+	for (i = 0; i < n; i++)
+	{
+		cout << A[i] << "\t";
+	}
+	for (i = 0; i < n; i++)
+	{
+		if (A[i] < -2 || A[i]>8)
+		{
+			A[i] += 10;
+		}
+	}
+	cout << "\nВывод измененного массива" << endl;
+	for (i = 0; i < n; i++)
+	{
+		cout << A[i] << "\t";
+	}
+
+
+
+
+
+}
