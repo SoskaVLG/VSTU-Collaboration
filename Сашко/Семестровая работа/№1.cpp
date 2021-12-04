531f2db3f91ff7e5b677f99cae454280cb403b12
Krykov Alexandr <krykov976@gmail.com>
2021-12-04 12:08:59 +0300

    Update №1.cpp

--- Сашко/Семестровая работа/№1.cpp
+++ Сашко/Семестровая работа/№1.cpp
@@ -1,19 +1,20 @@
 #include <iostream>
 #include <math.h>
 
-using namespace std; 
+using namespace std;
 
-int main () 
+int main()
 {
 	setlocale(LC_ALL, "rus");
-	double x, y, c = 0; 
+	double x, y, c = 0;
+	//ввод y и x с клавиатуры
 	cout << "\nВведите x\n> ";
-	cin >> x; 
+	cin >> x;
 	cout << "\nВведите y\n> ";
 	cin >> y;
+	//вычисление C согласно уловию задачи
 	if (x - y == 0) c = pow(x, 2) + pow(y, 2) + sin(x);
 	else if (x - y > 0) c = pow(x - y, 2) + cos(x);
 	else if (x - y < 0) c = pow(y - x, 2) + tan(x);
 	cout << "\nC = " << c << "\n";
-	system("pause"); 
 }
